2444def04b28e26797028a3d2d951e71ed8b63ba
nominik-spb <nominik.spb@gmail.com>
2025-11-25 18:50:41 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,7 @@
 public class PhoneBook {
+
+    public int add(String name, String number) {
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int result = phoneBook.add("Иван Иванов", "8-999-123-45-67");
+        assertTrue("Метод add должен возвращать количество номеров", result > 0);
+    }
 }
