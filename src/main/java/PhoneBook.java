85cb230fe06d20f9cae381cfaecaec1ddac48009
nominik-spb <nominik.spb@gmail.com>
2025-11-26 10:26:41 +0300

    printAllNames implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,12 +1,11 @@
 import jdk.internal.access.JavaSecurityAccess;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class PhoneBook {
 
     HashMap<String, String> numberList = new HashMap<>();
-    HashMap<String, String> nameList = new HashMap<>();
+    SortedMap<String, String> nameList = new TreeMap<>();
 
     public int add(String number, String name) {
         numberList.put(number,name);
@@ -25,7 +24,14 @@ public class PhoneBook {
     }
 
     public String printAllNames() {
-
-        return null;
+        if(nameList.isEmpty()) {
+            return null;
+        }
+        else {
+            Set<String> keys = nameList.keySet();
+            String result = keys.toString();
+            System.out.println(result);
+            return result;
+        }
     }
 }
